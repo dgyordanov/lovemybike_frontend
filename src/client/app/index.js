f1a7fe47a88e1f26eb0665f97aa47fd694d61d1f
Diyan Yordanov <dgyordanov@gmail.com>
2016-10-19 11:41:04 +0200

    Added logger middleware to log every action and state change

--- src/client/app/index.js
+++ src/client/app/index.js
@@ -2,15 +2,21 @@ import 'babel-polyfill'
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { applyMiddleware, createStore } from 'redux'
 import { Router, Route, IndexRoute, browserHistory } from 'react-router'
 import { syncHistoryWithStore, routerReducer } from 'react-router-redux'
+import thunk from 'redux-thunk';
+import promise from 'redux-promise';
+import createLogger from 'redux-logger';
 import bikesApp from './reducers/bikesApp'
 import App from './components/App'
 import OffersRoot from './components/OffersRoot'
 import OfferDetails from './components/OfferDetails'
 
-let store = createStore(bikesApp)
+let store = createStore(
+	bikesApp,
+	applyMiddleware(thunk, promise, createLogger())
+);
 
 // Create an enhanced history that syncs navigation events with the store
 const history = syncHistoryWithStore(browserHistory, store)
