10e840e91d3b3a8345b150e74d802417529e3c68
dgyordanov@gmail.com <Dido1983>
2016-11-08 22:21:11 +0100

    Added gender filters when the offers are read from the server

--- src/client/app/actions/actions.js
+++ src/client/app/actions/actions.js
@@ -9,13 +9,19 @@ export const setVisibilityFilter = (filter) => {
 
 export const applyFilters = (filters) => {
 	return function (dispatch) {
+	    // convert filters object to gender query
+	    let filterQuery = Object.keys(filters)
+	        .map((key, index) => filters[key] ? key : "")
+	        .reduce(
+                function(result, filterValue){ return result + filterValue }, "");
+
 		dispatch({'type': 'OFFERS_LOADING_START'});
-		axios.get('https://lovemybike.herokuapp.com/offers')
+		let filterParam = filterQuery ? '?gender=' + filterQuery : '';
+		axios.get('https://lovemybike.herokuapp.com/offers' + filterParam)
 			.then((response) => {
 				dispatch({
 					'type': 'OFFERS_LOADED_SUCCESS',
-					offers: response.data,
-					filters
+					offers: response.data
 				})
 			})
 			.catch(err => {
--- src/client/app/reducers/offers.js
+++ src/client/app/reducers/offers.js
@@ -2,26 +2,9 @@ const offers = (state = [], action) => {
   // TODO - read the offers from a rest service
   switch (action.type) {
     case 'OFFERS_LOADED_SUCCESS':
-      debugger;
-      if (!action.filters.w && !action.filters.m && !action.filters.c){
-        return action.offers;
-      }
-
-      // TODO improve it
-      let result = action.offers;
-      if (!action.filters.w) {
-        result = result.filter(offer => offer.gender !== "w");
-      }
-      if (!action.filters.m) {
-        result = result.filter(offer => offer.gender !== "m");
-      }
-      if (!action.filters.c) {
-        result = result.filter(offer => offer.gender !== "c");
-      }
-      return result;
-
+      return action.offers;
     default:
-      return state
+      return []
 
   }
   return state;
