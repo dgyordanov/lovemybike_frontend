ad9c5b745df93191d1cdf82221a6f1818bd6e6f3
Diyan Yordanov <dgyordanov@gmail.com>
2017-01-12 17:33:56 +0100

    selectField refactoring

--- src/client/app/components/form/selectField.js
+++ src/client/app/components/form/selectField.js
@@ -1,18 +1,14 @@
 "use strict";
 import React from 'react'
 
-const selectField = ({ options, label, required, meta: { touched, error, warning } }) => {
-  var optionElements = options.map(option =>
-    <option value={option.value}>{option.label}</option>);
-  return (
+const selectField = ({ options, label, required, meta: { touched, error, warning } }) => (
   <div className={`form-group required ${touched && error ? 'has-error' : '' }`}>
     <label className="control-label">{label}</label>
       <select>
-        {optionElements}
+        {options.map(option => <option value={option.value}>{option.label}</option>)}
       </select>
       {touched && error && <span className="help-block">{error}</span>}
   </div>
-    );
-}
+);
 
 export default selectField;
