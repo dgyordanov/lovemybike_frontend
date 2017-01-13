551ae0e455e07c11c3879fb01d1bf20e4e1aa35a
Diyan Yordanov <dgyordanov@gmail.com>
2017-01-13 22:30:54 +0100

    Fix the css of the selectField component

--- src/client/app/components/form/selectField.js
+++ src/client/app/components/form/selectField.js
@@ -4,7 +4,7 @@ import React from 'react'
 const selectField = ({ options, label, required, meta: { touched, error, warning } }) => (
   <div className={`form-group required ${touched && error ? 'has-error' : '' }`}>
     <label className="control-label">{label}</label>
-      <select>
+      <select className="form-control">
         {options.map(option => <option value={option.value}>{option.label}</option>)}
       </select>
       {touched && error && <span className="help-block">{error}</span>}
