1553287f45023121a340197321e31c087c149d1f
Diyan Yordanov <dgyordanov@gmail.com>
2017-02-05 15:49:36 +0100

    Remove not relevant TODO

--- src/client/app/components/form/selectField.js
+++ src/client/app/components/form/selectField.js
@@ -4,7 +4,6 @@ import React from 'react'
 const selectField = ({ options, label, required, meta: { touched, error, warning } }) => (
   <div className={`form-group required ${touched && error ? 'has-error' : '' }`}>
     <label className="control-label">{label}</label>
-      // TODO: use the redux form select
       <select className="form-control">
         {options.map(option => <option value={option.value}>{option.label}</option>)}
       </select>
