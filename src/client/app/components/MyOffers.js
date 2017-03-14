80a7e64661a9eafaa80cca34a2fa3c279f92abbe
Diyan Yordanov <dgyordanov@gmail.com>
2017-03-14 22:30:55 +0100

    Add my offers title

--- src/client/app/components/MyOffers.js
+++ src/client/app/components/MyOffers.js
@@ -4,6 +4,9 @@ import MyOfferList from '../containers/MyOfferList'
 const MyOffers = () => (
   <div>
     <div className="container margin-top-15">
+        <div className="col-md-10 col-md-offset-1 text-center">
+            <div><h1>My Offers</h1></div>
+        </div>
     	<div className="col-md-10 col-md-offset-1">
     		<MyOfferList />
     	</div>
