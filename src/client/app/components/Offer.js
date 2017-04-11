e0ceed84b5bddb00365d63a78170d229491bcaed
Diyan Yordanov <dgyordanov@gmail.com>
2017-04-11 17:33:57 +0200

    Fix the offer images to be shown correctly using image0_s instead of image0

--- src/client/app/components/Offer.js
+++ src/client/app/components/Offer.js
@@ -2,10 +2,10 @@ import React, { PropTypes } from 'react'
 import { Link } from 'react-router'
 
 
-const Offer = ({ id, title, image0, price, description }) => (
+const Offer = ({ id, title, image0_s, price, description }) => (
 	<div className="col-sm-4 col-lg-4 col-md-4">
         <div className="thumbnail">
-            <img src={image0} alt="" />
+            <img src={image0_s} alt="" />
             <div className="caption">
                 <h4 className="pull-right">{price} &euro;</h4>
                 <h4><Link to={`/offer/${id}`}>{title}</Link></h4>
@@ -18,7 +18,7 @@ const Offer = ({ id, title, image0, price, description }) => (
 Offer.propTypes = {
     id: PropTypes.number.isRequired,
     title: PropTypes.string.isRequired,
-    image0: PropTypes.string.isRequired,
+    image0_s: PropTypes.string.isRequired,
     price: PropTypes.number.isRequired,
     description: PropTypes.string.isRequired
 }
